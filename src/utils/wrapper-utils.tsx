edc4120aaa1617a51bc585f9fdb3d8e549e11170
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2020-09-05 13:48:38 +0530

    review fixes

--- src/utils/wrapper-utils.tsx
+++ src/utils/wrapper-utils.tsx
@@ -1,7 +1,9 @@
+import { getCurrentMillis } from "./date-utils";
+
 export const wrapThrottle = (func: any, timeFrame: number) => {
     let lastTime = 0;
     return function (...args: any) {
-        const now = new Date().getTime();
+        const now = getCurrentMillis();
         if (now - lastTime >= timeFrame) {
             func(...args);
             lastTime = now;
